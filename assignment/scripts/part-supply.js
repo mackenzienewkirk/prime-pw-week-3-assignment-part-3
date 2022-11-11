2b442e7af443b1e01a2d8ab69b668bf0080bf4e2
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2022-11-11 10:46:30 -0600

    supplyChanges.push(25)

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -4,20 +4,33 @@ console.log('****** Part Supply *******');
 // 1. Create a variable called 'partsNeeded' with a value of the number 40 
 //    & console.log the variable
 console.log('1. Number of partsNeeded:');
+let partsNeeded = 40
+console.log(partsNeeded);
 
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
+let supplyChanges = [ 3, 5, -6, 0, 7, 11];
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
+console.log(supplyChanges[1]);
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
 console.log('4. Removed item:');
+supplyChanges = supplyChanges.pop
+console.log(supplyChanges);
+
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
+
+supplyChanges = [ 3, 5, -6, 0, 7, ];
+
+supplyChanges.push(25);
+console.log(supplyChanges);
+
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -43,3 +56,13 @@ console.log('8. Total supplies available is:');
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
+
+let part = 572
+let boxes = 0
+
+console. log ('Parts:', part, 'Boxes: ', boxes) ;
+while (part > 6) {
+        part -= 7;
+        boxes++;
+}
+console. log ('Parts unboxed:', part,"Boxes filled: boxes");
